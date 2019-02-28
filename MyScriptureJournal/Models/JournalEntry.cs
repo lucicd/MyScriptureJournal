4e59d028c702ade1207b5cd15a0f76257a5a92a5
Drazen Lucic <lucicd@hotmail.com>
2019-02-28 09:10:23 +0400

    Add ES, scaffolding

---
+++ MyScriptureJournal/Models/JournalEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyScriptureJournal.Models
+{
+    public class JournalEntry
+    {
+        public int ID { get; set; }
+        public string Book { get; set; }
+        public string Note { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ MyScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.JournalEntries
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Models.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Models.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<JournalEntry> JournalEntry { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            JournalEntry = await _context.JournalEntry.ToListAsync();
+        }
+    }
+}
